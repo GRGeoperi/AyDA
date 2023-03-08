cdfccde9948d9c038997bc6d51a18005cfb6d2e6
Geoperi Gonzalez Roldan <ggonzalezr2100@alumno.ipn.mx>
2023-03-07 23:29:03 -0600

    Ejercicio 1

--- PrimerParcial/recursiveFibonacciSeries.c
+++ PrimerParcial/recursiveFibonacciSeries.c
@@ -1,24 +1,8 @@
 /*  Objetivo:
     Implementar el algoritmo recursivo para hallar el enésimo término
     de la serie de Fibonacci. Al ser un algoritmo costoso en tiempo,
-    únicamente se probó con un rango de 5 a 55. Siendo 60, el valor
-    máximo que se intentó probar, alcanzando 2 horas de ejecución y,
-    finalmente, abortando el proceso.
-*/
-
-/*  Tabla de pruebas:
-    n = 5 -> 0.000003 s
-    n = 10 -> 0.000004 s
-    n = 15 -> 0.000009 s
-    n = 20 -> 0.000094 s
-    n = 25 -> 0.000960 s
-    n = 30 -> 0.009884 s
-    n = 35 -> 0.098389 s
-    n = 40 -> 1.114837 s
-    n = 45 -> 11.640281 s
-    n = 50 -> 128.980501 s
-    n = 55 -> 1342.172001 s
-    n = 60 -> - s
+    únicamente se probó con un rango de 0 a 56. El resultado se exportó
+    a un archivo de texto.
 */
 
 #include <stdio.h>
@@ -35,13 +19,48 @@ unsigned long RecursiveFibonacci(int n)
         return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
 }
 
+double TestingTimeFunction(int n)
+{
+    double timeSpent = 0;      // Tiempo de ejecución en segundos
+    clock_t begin = clock();    // Inicio del conteo de tiempo
+    RecursiveFibonacci(n);     // Llamada a la función recursiva
+    clock_t end = clock();      // Fin del conteo de tiempo
+    timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
+    return timeSpent;          // Regresa el tiempo de ejecución
+}
+
+void ExportData(int n, double ArrayTime[])
+{
+    FILE* archivo;
+    archivo = fopen("TiempoRequerido.txt", "w");
+
+    if (archivo == NULL) 
+    {
+        printf("No se pudo crear el archivo");
+        return;
+    }
+    fprintf(archivo, "Entrada (n) | Tiempo Requerido (s)\n");
+    fprintf(archivo, "------------------------------------\n");
+    for (int i = 0; i <= n; i++) 
+    {
+        fprintf(archivo, "\t%d \t\t| \t\t%f\n", i, ArrayTime[i]);
+    }
+    fclose(archivo);
+}
+
+void TestAndExport()
+{
+    int n = 25; // Rango de prueba
+    double arrayTime[n];
+    for (int i = 0; i <= n; i++)
+    {
+        arrayTime[i] = TestingTimeFunction(i);
+    }
+    ExportData(n , arrayTime);
+}
+
 int main(void)
 {
-    clock_t begin = clock();    // Inicio del conteo de tiempo
-    RecursiveFibonacci(55);     // Llamada a la función recursiva
-    clock_t end = clock();      // Fin del conteo de tiempo
-
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; // Tiempo de ejecución en segundos
-    printf("Tiempo de ejecución: %f\n", time_spent);            // Impresión del tiempo de ejecución
+    TestAndExport();
     return 0;
 }
