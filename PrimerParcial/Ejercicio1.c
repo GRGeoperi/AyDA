a9e2a0d0930c2b80b002e83b75e9b81af5be49a1
Geoperi Gonzalez Roldan <ggonzalezr2100@alumno.ipn.mx>
2023-03-08 02:16:47 -0600

    Fibonacci recursivo

---
+++ PrimerParcial/Ejercicio1.c
@@ -0,0 +1,17 @@
+/*  Objetivo:
+    Implementar el algoritmo recursivo para hallar el enésimo término
+    de la serie de Fibonacci. Al ser un algoritmo costoso en tiempo,
+    únicamente se probó con un rango de 0 a 56. Los resultados se 
+    exportan a un archivo de texto.
+*/
+
+#include "recursiveFibonacciSeries.h"
+
+// ¡Peligro! Sobrepasar 56 puede causar más de una hora de ejecución.
+#define N 56
+
+int main(void)
+{
+    TestAndExport(N);
+    return 0;
+}
