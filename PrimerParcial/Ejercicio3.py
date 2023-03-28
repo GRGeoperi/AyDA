9e50a3bdbbe8a5c4c4d793372d55cb8b6dc9d801
Geoperi Gonzalez Roldan <ggonzalezr2100@alumno.ipn.mx>
2023-03-28 14:37:07 -0600

    Potencia recursiva y potencia logarítmica

--- PrimerParcial/Ejercicio3.py
+++ PrimerParcial/Ejercicio3.py
@@ -1,5 +1,15 @@
-def power(x, n):
+def power_r(x, n):
     if n == 0:
         return 1
     else:
-        return x * power(x, n-1)
+        return x * power_r(x, n-1)
+
+def power_l(x, n):
+    if n < 0:
+        return power_l(1 / x, -n)
+    if n == 0:
+        return 1
+    if (n % 2) == 0:
+        return power_l(x * x, n / 2)
+    else:
+        return power_l(x * x, (n - 1) / 2) * x
