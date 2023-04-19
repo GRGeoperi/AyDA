aba5717b60d17125ad7f6eb0668cd6abc9824965
Geoperi Gonzalez Roldan <ggonzalezr2100@alumno.ipn.mx>
2023-04-19 02:00:57 -0600

    Ordenamiento de un arreglo ya ordenado a la inversa

---
+++ SegundoParcial/Ejercicio7b.c
@@ -0,0 +1,44 @@
+#include "mergeSortDescendente.h"
+#include "mergeSortAscendente.h"
+#include "randomDynamicArray.h"
+
+int main(void)
+{
+    // Semilla para números aleatorios
+    srand(time(NULL));
+
+    // Variables de entrada
+    int size;
+    int randomnessRange;
+    int* test;
+
+    // Tamaño del arreglo
+    printf("Cantidad de valores del arreglo: ");
+    scanf("%d", &size);
+
+    // Rango de llenado
+    printf("Rango de llenado: ");
+    scanf("%d", &randomnessRange);
+
+    // Reservación de memoria para el arreglo
+    test = (int*)malloc(sizeof(int)*size);
+
+    // Creación del arreglo dinámico
+    test = CreateDynamicArray(size, randomnessRange);
+
+    // Muestra de arreglo desordenado
+    printf("Arreglo desordenado: ");
+    DisplayArray(test, size);
+
+    // Ordenamiento descendente
+    printf("Arreglo ordenado (mayor a menor): ");
+    MergeSortD(test, 0, size-1);
+    DisplayArray(test, size);
+
+    // Ordenamiento ascendente
+    printf("Arreglo ordenado (menor a mayor): ");
+    MergeSortA(test, 0, size-1);
+    DisplayArray(test, size);
+
+    return 0;
+}
