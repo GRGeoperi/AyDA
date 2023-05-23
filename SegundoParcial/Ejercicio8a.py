a70471af15d1f3c50fb25d8479750e7b887f89b6
Geoperi González Roldán <ggonzalezr2100@alumno.ipn.mx>
2023-05-22 23:35:19 -0600

    Tareas encontradas por fuerza bruta (#2)

--- SegundoParcial/Ejercicio8a.py
+++ SegundoParcial/Ejercicio8a.py
@@ -15,9 +15,9 @@ def print_dictionary(Dictionary):
 def bruteForce_dictionary(N, Dictionary, Interval):
     bestCombination = None
     maxDuties = 0
-    for i in range(1, N + 1):
+    for i in range(1, N + 1): # O(n)
         # Generar todas las combinaciones posibles
-        ''' combinaciones es una tupla (lista inmutable)
+        ''' combinations es una tupla (lista inmutable)
             que se saca con la fórmula nCr, donde:
             - n es el número total de elementos dentro del diccionario
             - r es el número de elementos seleccionados que en este caso es i
@@ -40,16 +40,16 @@ def bruteForce_dictionary(N, Dictionary, Interval):
             combinaciones = [9C1, 9C2, 9C3, ..., 9C9]
             combinaciones = [9, 36, 84, ..., 1]
         '''
-        combinaciones = itertools.combinations(Dictionary.items(), i)
+        combinations = itertools.combinations(Dictionary.items(), i) # O(2^n)
         # Probar cada combinación
-        ''' combinacion es cada uno de los elementos en la tupla de combinaciones,
+        ''' combination es cada uno de los elementos en la tupla de combinaciones,
             es decir:
             9C1 = [A,B,C,D,E,F,G,H,I] = 9
             9C2 = [AB,AC,AD,AE,AF,AG,AH,AI,BC,...,HI] = 36
             ...
             9C9 = [ABCDEFGHI] = 1
         '''
-        for combinacion in combinaciones:
+        for combination in combinations: # O(n)
             # Se suma la duración de cada combinación
             ''' es decir que suma_duracion, será la suma de todas las duraciones de aquellos
                 pendientes dentro de la combinacion actual:
@@ -57,17 +57,17 @@ def bruteForce_dictionary(N, Dictionary, Interval):
                 ...
                 9C9 = [ABCDEFGHI->2+5+3+...+1] = 38
             '''
-            suma_duracion = sum([duracion for _, duracion in combinacion])
+            suma_duracion = sum([duracion for _, duracion in combination]) # O(n)
             # Verificar si la combinación es válida y maximiza el número de pendientes
             ''' Esa combinacion elegida será válida únicamente cuando el Intervalo de tiempo
                 recibido sea mayor o igual a la suma de las duraciones de aquella
                 combinacion actual y además tal combinación maximice los pendientes hechos.
             '''
-            if Interval >= suma_duracion and len(combinacion) > maxDuties:
-                maxDuties = len(combinacion)
-                bestCombination = combinacion
+            if Interval >= suma_duracion and len(combination) > maxDuties:
+                maxDuties = len(combination) # O(1)
+                bestCombination = combination # O(1)
     # Devolver la mejor combinación encontrada
-    return dict(bestCombination) if bestCombination else {}
+    return dict(bestCombination) if bestCombination else {} # O(1)
 
 n = int(input("número de pendientes: "))
 periodo = int(input("intervalo de tiempo: "))
